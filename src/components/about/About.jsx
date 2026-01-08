bfc709220b3a5d0e4eebd0baa9d699901b197b7d
Roy Duenas <roy.duenas.sdtpnoli@gmail.com>
2026-01-09 00:23:14 +0800

    dynamic year of experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,6 +5,10 @@ import Separator from "../Separator";
 import { RxCaretRight } from "react-icons/rx";
 
 function About() {
+    const year = new Date().getFullYear();
+    const startYear = 2020;
+    const yearsOfExperience = year - startYear;
+
     return (
         <Container className="my-8" id="about">
             <div className="flex items-center gap-2 mb-8">
@@ -19,7 +23,7 @@ function About() {
                     <div className="experience-inner h-full rounded-xl p-4">
                         <div className="flex flex-col gap-4 h-full">
                             <div className="flex items-center font-bold">
-                                <div className="text-8xl">5+</div>
+                                <div className="text-8xl">{yearsOfExperience}+</div>
                                 <div>
                                     <div className="text-4xl">Years</div>
                                     <div className="text-xl">Experience</div>
