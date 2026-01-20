09978afd40613ab27c7d33286efc0d06094a84f7
Roy Duenas <roy.duenas.sdtpnoli@gmail.com>
2026-01-21 02:02:07 +0800

    Added breakthrough ticketing app

---
+++ src/data/projects/ticketing.js
@@ -0,0 +1,25 @@
+import homeImg from "../../assets/projects/ticketing/home.PNG";
+import buyTicketFormImg from "../../assets/projects/ticketing/buy-ticket-form.PNG";
+import findTicketsImg from "../../assets/projects/ticketing/find-tickets.PNG";
+import findTickets2Img from "../../assets/projects/ticketing/find-tickets-2.PNG";
+import adminTicketsListImg from "../../assets/projects/ticketing/admin-tickets-list.PNG";
+import perAreaImg from "../../assets/projects/ticketing/per-area.PNG";
+import updateTicketPricingImg from "../../assets/projects/ticketing/update-ticket-pricing.PNG";
+
+
+const ticketing_project = {
+    title: "BREAKTHROUGH TICKETING System",
+    imgSrc: homeImg,
+    description: "A simple web-based ticketing system for purchasing event tickets online, featuring user-friendly ticket selection, secure payment processing, and an admin dashboard for managing ticket inventory and pricing.",
+    previewImages: [
+        homeImg,
+        buyTicketFormImg,
+        findTicketsImg,
+        findTickets2Img,
+        adminTicketsListImg,
+        perAreaImg,
+        updateTicketPricingImg,
+    ]
+}
+
+export default ticketing_project;
