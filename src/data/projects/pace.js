4395452f03f22f088990d41096fc512c803a5f62
Roy Dueñas <rduenas@ingenuiti.com>
2026-01-21 12:16:19 +0800

    Added PACE

---
+++ src/data/projects/pace.js
@@ -0,0 +1,37 @@
+import adminDashboardImg from "../../assets/projects/pace/admin-dashboard.PNG";
+import adminDepartmentsImg from "../../assets/projects/pace/admin-departments.PNG";
+import adminUsersImg from "../../assets/projects/pace/admin-users.PNG";
+import adminEmployeesCreate2Img from "../../assets/projects/pace/admin-employees-create-2.PNG";
+import adminEmployeesCreate3Img from "../../assets/projects/pace/admin-employees-create-3.PNG";
+import adminEmployeesCreateImg from "../../assets/projects/pace/admin-employees-create.PNG";
+import adminEmployeesImg from "../../assets/projects/pace/admin-employees.PNG";
+import adminAgencyFeesImg from "../../assets/projects/pace/admin-agency-fees.PNG";
+import adminReportAgencyFeeImg from "../../assets/projects/pace/admin-report-agency-fee.PNG";
+import adminReportEmployeeRecordImg from "../../assets/projects/pace/admin-report-employee-record.PNG";
+import adminProfileImg from "../../assets/projects/pace/admin-profile.PNG";
+import userEmployeesListImg from "../../assets/projects/pace/user-employees-list.PNG";
+import userEmployeesContributionImg from "../../assets/projects/pace/user-employees-contribution.PNG";
+import userAgencyFeesImg from "../../assets/projects/pace/user-agency-fees.PNG";
+
+const asc_project = {
+    title: "Progressive Alliance of Capitol Employees",
+    imgSrc: adminDashboardImg,
+    description: "This web application is designed to securely record and manage employee contributions to the Progressive Alliance of Capitol Employees. It provides a centralized system for tracking contribution history, ensuring accuracy, transparency, and easy access to records. The platform streamlines data management, reduces manual errors, and supports efficient reporting for administrative and auditing purposes.",
+    previewImages: [
+        adminDepartmentsImg,
+        adminUsersImg,
+        adminEmployeesCreate2Img,
+        adminEmployeesCreate3Img,
+        adminEmployeesCreateImg,
+        adminEmployeesImg,
+        adminAgencyFeesImg,
+        adminReportAgencyFeeImg,
+        adminReportEmployeeRecordImg,
+        adminProfileImg,
+        userEmployeesListImg,
+        userEmployeesContributionImg,
+        userAgencyFeesImg,
+    ],
+};
+
+export default asc_project;
