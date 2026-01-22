c33c851dda49ca86366538e05e4f7a0a89626acc
Roy Dueñas <rduenas@ingenuiti.com>
2026-01-22 12:47:38 +0800

    Code clean up, move data related codes from jsx to data folder

--- src/components/projects-sample/ProjectSamples.jsx
+++ src/components/projects-sample/ProjectSamples.jsx
@@ -1,32 +1,9 @@
 import Button from "../Button";
 import Separator from "../Separator";
 import ProjectSampleItem from "./ProjectSampleItem";
-
-import captchaImg from "../../assets/projects/captcha.PNG";
-import upversionImg from "../../assets/projects/upversion.PNG";
-import everglowImg from "../../assets/projects/everglow.PNG";
 import { FadeInLeft, FadeInRight } from "../animations/Animations";
+import { sample_projects } from "../../data/projects-sample/project-sample-data";
 
-const sample_projects = [
-    {
-        title: "10Captcha Website",
-        imgSrc: captchaImg,
-        link: "https://gldrp.com/10captcha/",
-        linkDisplay: "gldrp.com/10captcha/",
-    },
-    {
-        title: "Up Version Website",
-        imgSrc: upversionImg,
-        link: "https://gldrp.com/upversion/#/",
-        linkDisplay: "gldrp.com/upversion/",
-    },
-    {
-        title: "Interactive Event Website",
-        imgSrc: everglowImg,
-        link: "https://yfceverglow.gldrp.com/",
-        linkDisplay: "yfceverglow.gldrp.com",
-    },
-];
 export default function ProjectSamples() {
     return (
         <div>
---
+++ src/data/projects-sample/project-sample-data.js
@@ -0,0 +1,28 @@
+
+
+import captchaImg from "../../assets/projects/captcha.PNG";
+import upversionImg from "../../assets/projects/upversion.PNG";
+import everglowImg from "../../assets/projects/everglow.PNG";
+
+const sample_projects = [
+    {
+        title: "10Captcha Website",
+        imgSrc: captchaImg,
+        link: "https://gldrp.com/10captcha/",
+        linkDisplay: "gldrp.com/10captcha/",
+    },
+    {
+        title: "Up Version Website",
+        imgSrc: upversionImg,
+        link: "https://gldrp.com/upversion/#/",
+        linkDisplay: "gldrp.com/upversion/",
+    },
+    {
+        title: "Interactive Event Website",
+        imgSrc: everglowImg,
+        link: "https://yfceverglow.gldrp.com/",
+        linkDisplay: "yfceverglow.gldrp.com",
+    },
+];
+
+export {sample_projects};
